f8e9048b48b787928c49e8f240449864c0cf2e9a
Robertni <robert@3sam.io>
2023-10-24 14:10:43 +0700

    build: related stuff for publishing npm package

---
+++ src/elements/Button/index.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>Hello Elusive Button</button>
+}
+
+export default Button
---
+++ src/elements/SimpleComponent/index.tsx
@@ -0,0 +1,5 @@
+const SimpleComponent = () => {
+  return <div>Hello! Welcome to the world.</div>
+}
+
+export default SimpleComponent
